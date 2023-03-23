09c18c9da3600abf86ec75f53fbd5cdf99961378
mariajosemrt <andreumj7@gmail.com>
2023-03-23 14:13:08 +0100

    Estado inicial del proyecto

---
+++ src/main/java/com/ejercicio/SpringMvccrudEmpresaApplication.java
@@ -0,0 +1,13 @@
+package com.ejercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvccrudEmpresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvccrudEmpresaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ejercicio/SpringMvccrudEmpresaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvccrudEmpresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
